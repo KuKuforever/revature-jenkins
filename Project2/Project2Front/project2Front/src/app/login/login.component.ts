c52b6e9506da3d980656c84ecd44f3e8bd4f9d33
kukuforever <kanxue0012@gmail.com>
2017-11-21 07:07:56 -0500

    fixed the alignment for each comoponent

--- Project2/Project2Front/project2Front/src/app/login/login.component.ts
+++ Project2/Project2Front/project2Front/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {HttpClient} from '@angular/common/http';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 
 @Component({
@@ -9,16 +10,23 @@ import {HttpClient} from '@angular/common/http';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  username = '';
-  password = '';
+  email: string = '';
+  password: string = '';
+  form: FormGroup;
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router,
+              private fb: FormBuilder) {
+    this.form = fb.group({
+      vemail: [null, [Validators.required]],
+      vpassword: [null, [Validators.required]]
+    });
+  }
 
   ngOnInit() {
   }
 
   login() {
-    let account = {'username': this.username, 'password': this.password};
+    let account = {'username': this.email, 'password': this.password};
     this.http.post('', account, {withCredentials: true});
     this.router.navigate([('home')]);
   }
