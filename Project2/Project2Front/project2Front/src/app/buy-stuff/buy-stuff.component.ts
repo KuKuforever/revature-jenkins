fe51099672bf69f45f79aa76d8a0e4eb182ff570
kukuforever <kanxue0012@gmail.com>
2017-12-05 10:48:02 -0500

    angular material table implemented on every tables

--- Project2/Project2Front/project2Front/src/app/buy-stuff/buy-stuff.component.ts
+++ Project2/Project2Front/project2Front/src/app/buy-stuff/buy-stuff.component.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {Router} from '@angular/router';
 import {Post} from "../models/post";
 import {HttpClient} from "@angular/common/http";
 import {PostService} from "../post.service";
+import {MatPaginator, MatSort, MatTableDataSource} from "@angular/material";
 
 @Component({
   selector: 'app-buy-stuff',
   templateUrl: './buy-stuff.component.html',
   styleUrls: ['./buy-stuff.component.css']
 })
-export class BuyStuffComponent implements OnInit {
+export class BuyStuffComponent implements OnInit, AfterViewInit {
   wantPosts: Post[];
   url = 'http://localhost:8085/post/wantPost';
+
+  displayedColumns1 = ['postId', 'title', 'typeId.type', 'statusId.status', 'postDate'];
+  dataSource1 = new MatTableDataSource(this.wantPosts);
+
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private http: HttpClient,
               private router: Router,
@@ -19,6 +26,13 @@ export class BuyStuffComponent implements OnInit {
 
   ngOnInit() {
     this.getWantPost();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource1  = new MatTableDataSource(this.wantPosts);
+    this.dataSource1.paginator = this.paginator;
+    this.dataSource1.sort = this.sort;
+    this.dataSource1  = new MatTableDataSource(this.wantPosts);
   }
 
   newBuyPost() {
@@ -30,6 +44,10 @@ export class BuyStuffComponent implements OnInit {
     this.http.get<Post[]>(this.url, {withCredentials: true})
       .subscribe((data) => {
         this.wantPosts = data;
+        this.dataSource1  = new MatTableDataSource(this.wantPosts);
+
+        this.dataSource1.paginator = this.paginator;
+        this.dataSource1.sort = this.sort;
         console.log(this.wantPosts);
       }, (err) => {
         console.log(err);
@@ -39,6 +57,13 @@ export class BuyStuffComponent implements OnInit {
     console.log(postId);
     this.postService.changePostId(postId);
     this.router.navigate([('viewPost')]);
+  }
+
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.dataSource1.filter = filterValue;
   }
 
 }
